ad1b221b37a49df49f0d0c1766a3da586e499020
Marlon Torres <marlont@bluetrailsoft.com>
2022-06-28 15:33:11 -0500

    feat: welcome page added

--- app/root.tsx
+++ app/root.tsx
@@ -2,53 +2,96 @@ import type {
   LinksFunction,
   LoaderFunction,
   MetaFunction,
-} from "@remix-run/node";
-import { json } from "@remix-run/node";
+} from '@remix-run/node'
+import { json } from '@remix-run/node'
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  useLoaderData,
+} from '@remix-run/react'
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
+import globalStylesheetUrl from './styles/global.css'
+import { getUser } from './session.server'
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
+  return [{ rel: 'stylesheet', href: globalStylesheetUrl }]
+}
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'Easy Shopping',
+})
 
 type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
+  user: Awaited<ReturnType<typeof getUser>>
+}
+
+interface ParentElement {
+  children: JSX.Element
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   return json<LoaderData>({
     user: await getUser(request),
-  });
-};
+  })
+}
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children }: ParentElement) {
+  return (
+    <html lang="en">
       <head>
         <Meta />
         <Links />
+        <title>Easy Shopping</title>
       </head>
-      <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
-  );
+  )
+}
+
+function Layout({ children }: ParentElement) {
+  const { user } = useLoaderData()
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Easy Shopping
+        </Link>
+
+        <ul className="nav">
+          {user ? (
+            <li>
+              <form action="/auth/logout" method="POST">
+                <button className="btn" type="submit">
+                  Logout {user.username}
+                </button>
+              </form>
+            </li>
+          ) : (
+            <li>
+              <Link to="/auth/login">Login</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
+  )
 }
